6a92d4215e0600e64a129eeb322144e346b7cdbe
iamsss <saurav9760@gmail.com>
2018-12-29 18:34:00 +0530

    Adding Event listener

--- js/index.js
+++ js/index.js
@@ -1,15 +1,20 @@
 new Vue({
-    el:'#vue-app',
+    el: '#vue-app',
     data: {
-        name: 'Saurav Kumar',
-        data: 'D1',
-        website: 'https://google.com',
-        htmlTag:'<h1> I am header</h1>'
+        age: 25,
+        x: 0,
+        y: 0
     },
     methods: {
-        greet: function(){
-
-            return 'Welcome -> ' + this.name;
+        add: function(inc){
+            this.age += inc;
+        },
+        subtract: function(dec){
+            this.age -= dec;
+        },
+        updateXY: function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 });
